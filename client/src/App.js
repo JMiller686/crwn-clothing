e8958c830cb8621023a2ec41ecbf162efb47d9f5
JMiller686 <jmiller686@gmail.com>
2021-07-21 11:43:05 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import { checkUserSession } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
 const App = ({ checkUserSession, currentUser }) => {
-
+  console.log('TEST');
   useEffect(() => {
     checkUserSession();
   }, [checkUserSession]);
